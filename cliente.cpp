11b11b91e8a5c2c347401e67910f68284f847740
Rattay <federattay@gmail.com>
2019-06-17 17:22:11 -0300

    Elimine descuento de los clientes, pertenece a otra clase

--- cliente.cpp
+++ cliente.cpp
@@ -3,7 +3,6 @@
 Cliente::Cliente()
 {
 	numero = 0;
-	descuento = 0;
 }
 void Cliente::asignarNumero(int _numero)
 {
@@ -12,14 +11,6 @@ void Cliente::asignarNumero(int _numero)
 int Cliente::obtenerNumero()
 {
 	return numero;
-}
-void Cliente::asignarDescuento(double _descuento)
-{
-	decuento = _descuento;
-}
-double Cliente::obtenerDescuento()
-{
-	return descuento;
 }
 Cliente::~Cliente()
 {
--- cliente.hpp
+++ cliente.hpp
@@ -9,15 +9,11 @@ class Cliente
 
 	protected:
 			int numero;
-			double descuento;
 	public:
 			Cliente();
 			
 			void asignarNumero(int _numero);
 			int obtenerNumero();
-			
-			void asignarDescuento(double _descuento);
-			double obtenerDescuento();
 			
 			~Cliente();
 };
