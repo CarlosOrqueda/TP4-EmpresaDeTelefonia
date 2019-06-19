da1917fc4d3a13636dea35d4527aec499c799545
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2019-06-19 16:00:57 -0300

    Se modificaron metodos cliente, solo tiene set y get, no se necesita constructor

--- cliente.cpp
+++ cliente.cpp
@@ -1,22 +1,15 @@
 #include "cliente.hpp"
 
-Cliente::Cliente():Precio()
-{
-	numero = 0;
-}
-Cliente::Cliente(double _precio,double _descuento,int _numero):Precio(_precio,_descuento)
-{
-	numero = _numero;
-}
+
 void Cliente::asignarNumero(int _numero)
 {
-	numero = _numero;
+	numeroTelefono = _numero;
 }
 int Cliente::obtenerNumero()
 {
-	return numero;
+	return numeroTelefono;
 }
-/*Cliente::~Cliente()
+Cliente::~Cliente()
 {
 	
-}*/
+}
